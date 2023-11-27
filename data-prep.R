42d430877278fbd3533d3524c42fc70665ad4ee3
Fawcett <T.W.Fawcett@exeter.ac.uk>
2023-11-27 09:36:01 +0000

    Init project Winner & Loser Effects in eSports

---
+++ data-prep.R
@@ -0,0 +1,10 @@
+require(tidyverse)
+WLdata <- read.csv("csgo_data_main.csv",na.strings=c(""," ","NA"))
+WLdata <- as_tibble(WLdata)
+WLdatalong <- WLdata %>%
+  pivot_longer(cols = contains(".winner.id"),
+               names_to = "game",
+               names_pattern = "game_(.*).winner.id",
+               values_to = "game_winner_id",
+               values_drop_na = TRUE
+  )
