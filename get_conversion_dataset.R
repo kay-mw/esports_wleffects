7662b5a8cb889be70fe0cbe8b74c8117ae9c2741
KW Malhotra <142889894+kay-mw@users.noreply.github.com>
2024-03-16 11:18:49 +0000

    Delete get_conversion_dataset.R

--- get_conversion_dataset.R
+++
@@ -1,8 +0,0 @@
-url <- "https://data.bis.org/static/bulk/WS_XRU_csv_flat.zip"
-file_name <- "WS_XRU_csv_flat.zip"
-
-download.file(url, paste(file_name, sep = ""), mode = "wb")
-
-unzip(file_name)
-
-file.rename("WS_XRU_csv_flat.csv","historical_exchange_rates.csv")
